e8de2341ff8320f53460476a19c49944002baf0f
Lleïr Borràs Metje <lleir@llegeix.me>
2018-01-04 16:19:17 +0100

    make to_prepare compatible with rails 5

--- init.rb
+++ init.rb
@@ -34,7 +34,7 @@ Redmine::Plugin.register :redmine_rubycas do
   })
 end
 
-ActionDispatch::Callbacks.to_prepare do
+ActiveSupport::Reloader.to_prepare do
   require_dependency 'account_controller'
   require_dependency 'setting'
   AccountController.send(:include, AccountControllerPatch)
